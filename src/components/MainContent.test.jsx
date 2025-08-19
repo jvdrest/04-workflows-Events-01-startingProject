504abe8c04635a29f4328e6ec17a30f379b62554
jvdrest <jvdrest@hotmail.com>
2025-08-19 11:25:47 +0200

    added comments [skp ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // The tests
+// Yet another comment
 
 describe('MainContent', () => {
   it('should render a button', () => {
